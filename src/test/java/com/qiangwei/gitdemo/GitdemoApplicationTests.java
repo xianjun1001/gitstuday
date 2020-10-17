4eadb5c09f76572823a5762451a75f115b84d521
xianjun1001 <13043415176@163.com>
2020-10-18 01:12:02 +0800

    1111

---
+++ src/test/java/com/qiangwei/gitdemo/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiangwei.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
